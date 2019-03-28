7f97fda85d16a3d68c21ebec79d90b16c983a146
hekangning <hekangning@makeblock.com>
2019-03-28 10:49:40 +0800

    添加很多东西

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"log"
-	"time"
+	"os"
 )
 
 var db *gorm.DB
@@ -18,11 +18,12 @@ type Model struct {
 
 func init() {
 	var err error
-	db, err = gorm.Open(setting.DatabaseSetting.Type, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		setting.DatabaseSetting.User,
-		setting.DatabaseSetting.Password,
-		setting.DatabaseSetting.Host,
-		setting.DatabaseSetting.Name))
+	db, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME")))
 
 	if err != nil {
 		log.Fatalf("models.Setup err: %v", err)
